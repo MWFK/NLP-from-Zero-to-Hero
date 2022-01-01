81d601bf1dad9172e0d2b4474ffc72512f7a30c9
MWFK <mouafek.ayadi@esprit.tn>
2022-01-01 19:22:43 +0100

    Update Webhose_Scraping.py

--- scikit-learn/News Scraping & Modeling/Webhose_Scraping.py
+++ scikit-learn/News Scraping & Modeling/Webhose_Scraping.py
@@ -8,16 +8,16 @@ os.environ["HTTPS_PROXY"] = PROXY
 # search = {'language':'german', 
 #           'thread.country':'DE', 
 #           'site_type':'news', 
-#           'thread.title':('übernimmt OR kauft OR übernahme OR übergibt OR veräuss* OR erwir* OR erwerb* OR kauft OR  verkauft OR schluck* OR kaufen OR beteilig* OR "hört auf" OR *beteiligung'),  
-#           'text':('Mitarbeiter OR Arbeitsplätze OR Angestellte OR Mitarbeitende OR immobilien OR Verlust OR gewinn OR umsatz OR firma OR unternehmen OR sitz OR gruppe OR group OR Mittelstand OR mittelständisch'), 
+#           'thread.title':(' *** OR ***'),  
+#           'text':(' *** OR ***'), 
 #           'is_first':'true',
-#           #'published':'>1564610400000',
+#           #'published':'>**',
 #           }
 
-# token   = '2bf759c0-3866-4ff6-a219-82badc4ad312'
+# token   = '**'
 # webhoseio.config(token=token)
 # https://webhose.io/web-content-api
-output = webhoseio.query('filterWebContent?token=***&format=json&ts=1629118130768&sort=crawled&q=language%3Agerman%20thread.country%3ADE%20site_type%3Anews%20thread.title%3A(%C3%BCbernimmt%20OR%20kauft%20OR%20%C3%BCbernahme%20OR%20%C3%BCbergibt%20OR%20ver%C3%A4uss*%20OR%20erwir*%20OR%20erwerb*%20OR%20kauft%20OR%20verkauft%20OR%20schluck*%20OR%20kaufen%20OR%20beteilig*%20OR%20%22h%C3%B6rt%20auf%22%20OR%20*beteiligung)%20text%3A(Mitarbeiter%20OR%20Arbeitspl%C3%A4tze%20OR%20Angestellte%20OR%20Mitarbeitende%20OR%20immobilien%20OR%20Verlust%20OR%20gewinn%20OR%20umsatz%20OR%20firma%20OR%20unternehmen%20OR%20sitz%20OR%20gruppe%20OR%20group%20OR%20Mittelstand%20OR%20mittelst%C3%A4ndisch)%20is_first%3Atrue%20published%3A%3E1564610400000')
+output = webhoseio.query('filterWebContent?token=***&format=json&***')
 print(output)
 
 # while int(output['moreResultsAvailable'])>0:
